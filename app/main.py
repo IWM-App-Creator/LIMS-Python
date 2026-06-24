9c7c7b30d27a548b26f8495ed08aa9b5ee24a711
Dipak Bera <dipakberait@gmail.com>
2026-06-24 17:34:31 +0530

    Update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,3 @@
-# from fastapi import FastAPI
-# from app.routes.users import router as user_router
-
-# app = FastAPI()
-
-# app.include_router(user_router)
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.users import router as user_router
@@ -13,8 +6,11 @@ from app.routes.barcode import router as barcode_router
 
 from app.firebase_service import init_firebase
 
-
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "True"}
 
 @app.on_event("startup")
 async def startup_event():
