23538d589da382a1f3f2e6a49cb87cbd447f75b8
Dipak Bera <dipakberait@gmail.com>
2026-07-03 18:49:02 +0530

    Update

--- app/httphelper/publicendpoints.py
+++ app/httphelper/publicendpoints.py
@@ -2,11 +2,13 @@ PUBLIC_APIS = {
     "/docs",
     "/openapi.json",
     "/favicon.ico",
+    "/backend/docs",
 }
 
 PUBLIC_PREFIXES = (
     "/api/v1/auth/",
     "/redoc",
+    "/backend/api/v1/auth/",
 )
 
 def isPublicEndpoint(path: str) -> bool:
