5798ccc1721cc12d0876913b3ba0c9a93a23d5c2
IWM-App-Creator <chintan.sarda@portalsolutions.in>
2026-07-07 11:42:34 +0530

    Misc changes

--- app/dbfunctions/viewfunctions.py
+++ app/dbfunctions/viewfunctions.py
@@ -5,7 +5,11 @@ def getViewDataByID(viewps):
     view_id = viewps.view_id.get()
     tblview = DB.getTableMeta("sys_new_dynamic_view").alias("tblcols")
     stmt = (select(tblview)).where(tblview.c.is_delete == 0)
+    is_single = 0
     if view_id not in (None, "", 0):
         stmt = stmt.where(tblview.c.view_id == view_id)
-        return DB.executeDBSelectSingle(stmt) # Return Single Value 
-    return DB.executeDBSelect(stmt) # Return Array Value 
+        is_single = 1
+    if is_single == 1 : # Return Single Value 
+        return DB.executeDBSelectSingle(stmt)
+    else : # Return Array Value 
+        return DB.executeDBSelect(stmt)
