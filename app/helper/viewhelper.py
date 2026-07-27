6cce1f0b2554f981a9fe23728f77cb845f650ae6
IWM-App-Creator <chintan.sarda@portalsolutions.in>
2026-07-27 15:58:39 +0530

    Misc Changes

--- app/helper/viewhelper.py
+++ app/helper/viewhelper.py
@@ -480,6 +480,7 @@ class ViewHelper:
                 dbcol = f"{col_id}{col_name}_{qry_alias}"
                 dbval = getattr(data, dbcol, "")
                 if col_type in ("DATETIME", "DATE", "TIME"):
+                    item[f"{col_id}|{col_name}lbl"] = str(dbval)
                     dbval = formatDate(from_date=str(dbval), format=col.get("date_format"))
                 item[f"{col_id}|{col_name}"] = str(dbval)
                 if (dbhlp.isUserColumn(col_name, 0) or (col_type in ("MAPCOL", "DISPLAYAS") and int(col.get("lookup_colid", 0)) > 0)):
