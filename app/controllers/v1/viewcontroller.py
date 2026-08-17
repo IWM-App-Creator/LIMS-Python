7ac725e4bdc8668b7d7935c6b696ad2829d5fe4c
RushiChandra472 <rushichandra472@gmail.com>
2026-08-17 16:20:37 +0530

    Delete Qry Changs

--- app/controllers/v1/viewcontroller.py
+++ app/controllers/v1/viewcontroller.py
@@ -43,7 +43,7 @@ def getViewData(request: Request):
         view_qry = viewps.view_qry.get() # Get Query
         # check Main Table Delete Records
         if viewps.show_deleted.get() in (0, "0", None):
-            view_qry = view_qry + " AND is_delete = 0"
+            view_qry = view_qry + " AND mtbl.is_delete = 0"
         # set Filter Qry by calling from Dashboard
         if viewps.call_from.get() == "Dashboard":
             dps.dashboard_id.set(params.get("dashboard_id"))
