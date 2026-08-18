745d4371690a9fe69163a7f4cba2b1dfc8ad82c3
RushiChandra472 <rushichandra472@gmail.com>
2026-08-18 09:54:24 +0530

    View CSV Chanhes

--- app/endpoints/v1/viewcsv.py
+++ app/endpoints/v1/viewcsv.py
@@ -4,8 +4,8 @@ from app.controllers.v1 import viewcsvcontroller as viewcsvapi
 router = APIRouter(prefix = "/viewcsv")
 
 ROUTES = [
-    ("download", viewcsvapi.downloadCSVData, ["GET"]),
-    ("upload", viewcsvapi.uploadCSVData, ["GET"])
+    ("/download", viewcsvapi.downloadCSVData, ["GET"]),
+    ("/upload", viewcsvapi.uploadCSVData, ["GET"])
 ]
 
 for path, handler, methods in ROUTES:
