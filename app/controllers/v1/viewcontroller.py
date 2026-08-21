ce43070178445700fad1ed224cef5790c163d3d0
RushiChandra472 <rushichandra472@gmail.com>
2026-08-21 17:01:45 +0530

    Misc Chnages

--- app/controllers/v1/viewcontroller.py
+++ app/controllers/v1/viewcontroller.py
@@ -118,7 +118,6 @@ def getViewData(request: Request):
             content = {
                 "status": False,
                 "message": "View Error",
-                "view_name": viewps.view_name.get(),
                 "user_id": userps.user_id.get(),
                 "ws_role_id": userps.ws_role_id.get(),
                 "workspace_id": userps.workspace_id.get(),
